1419eb721f6f190914f487b1c2215fdfe86a21ed
Sasha <136343348+Sashits@users.noreply.github.com>
2023-09-15 09:17:54 -0300

    Add files via upload

---
+++ examen.JS Sasha/examen.js
@@ -0,0 +1,25 @@
+1
+function salarioJuan (num)
+{
+  console.log(num / 12)
+}
+salarioJuan(21600)
+
+
+2
+function salarioMarcos (numero)
+{ 
+    console.log(numero * 12)
+}
+salarioMarcos(1500)
+
+3
+
+
+let Nota1 = Number(prompt('Ingrese un número'))
+let Nota2 = Number(prompt('Ingrese un segundo número'))
+let Nota3 = Number(prompt('Ingrese un tercer número'))
+    let suma = Nota1 + Nota2 + Nota3
+    let promedioNota = suma / 3
+
+alert (promedioNota)
